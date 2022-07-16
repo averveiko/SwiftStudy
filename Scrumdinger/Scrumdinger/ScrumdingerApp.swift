3de3e1a1d5052743e6599a1c16c492ba49883aea
Alexander Verveyko <sashkam@MacBook-Air-Alexander.local>
2022-07-16 16:21:59 +0300

    Scrumdinger dev begin

---
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Alexander Verveyko on 16.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
